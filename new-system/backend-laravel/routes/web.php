739d2932251996addf662b35198234d26ff5c912
Francis Rosete <202312263@gordoncollege.edu.ph>
2026-08-05 07:38:16 +0800

    remove route/web.php

--- new-system/backend-laravel/routes/web.php
+++ new-system/backend-laravel/routes/web.php
@@ -9,24 +9,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/run-db-seed-999', function () {
-    try {
-        Artisan::call('migrate', ['--force' => true]);
-
-        $user = User::updateOrCreate(
-            ['email' => 'nurse@gordoncollege.edu.ph'],
-            [
-                'name'              => 'Nurse Admin',
-                'password'          => Hash::make('password123'),
-                'role'              => 'CLINIC_STAFF',
-                'clinic_staff_type' => 'NURSE',
-            ]
-        );
-
-        return "Success! User created: " . $user->email;
-    } catch (\Throwable $e) {
-        return '<strong>Database Error:</strong> ' . $e->getMessage();
-    }
-});
 
 
