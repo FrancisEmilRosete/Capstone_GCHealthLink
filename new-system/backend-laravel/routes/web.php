70418100dfd491c06a480cda602f00508a95d17c
Francis Rosete <202312263@gordoncollege.edu.ph>
2026-08-05 07:52:13 +0800

    remove route/web.php

--- new-system/backend-laravel/routes/web.php
+++ new-system/backend-laravel/routes/web.php
@@ -1,18 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-Route::get('/run-db-seed-999', function () {
-    try {
-        Artisan::call('migrate:fresh', ['--seed' => true, '--force' => true]);
-
-        return 'Database wiped, re-migrated, and seeded successfully!';
-    } catch (\Throwable $e) {
-        return '<strong>Seeding Error:</strong> ' . $e->getMessage();
-    }
 });
