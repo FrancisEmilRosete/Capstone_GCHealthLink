95d5a6a77224cfa7bf4458ced6bcbd60e5c469cf
Francis Rosete <202312263@gordoncollege.edu.ph>
2026-08-04 23:59:59 +0800

    seed db

--- new-system/backend-laravel/routes/web.php
+++ new-system/backend-laravel/routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+use Illuminate\Support\Facades\Artisan;
+
+Route::get('/run-db-seed-999', function () {
+    Artisan::call('migrate:fresh', ['--seed' => true, '--force' => true]);
+    return 'Database migrated and seeded successfully!';
+});
